25bcf1c32e7fd6bc6838db36049726508dacd4ea
pipecosme123 <pipecosme123@gmail.com>
2020-05-21 12:53:40 -0500

    Clases Curso, Pais, ControlPais Terminadas
    Clases finalizadas

--- Classy/src/control/ControlCurso.java
+++ Classy/src/control/ControlCurso.java
@@ -18,9 +18,17 @@ public class ControlCurso {
     public LinkedList<Curso> consultarTodoCurso() {
         LinkedList<Curso> listacurso = new LinkedList<>();
         Curso objcurso = new Curso();
-        String sql = "SELECT * FROM categorias;";
+        String sql = "select * from Cursos";
         listacurso = objcurso.consultAllCurso(sql);
         return listacurso;
+        
+        /*
+        String sql = "select c.id_curso as 'Id', c.nombre_curso as 'Cursos', c.descripcion_curso as 'Descripcion', "
+                + "a.nombre1_profesor as 'Nombre docente', a.apellido1_profesor as 'Apellido', "
+                + "b.nombre_categoria as 'Categoria' from CURSOS as c "
+                + "inner join PROFESORES as a on a.id_profesor = c.id_profesorF "
+                + "inner join Categorias as b on b.id_categoria = c.id_categoriaF;";
+        */
     }
 
 }

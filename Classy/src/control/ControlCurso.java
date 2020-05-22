c934a6b49c98009143e8af6e6966ea2d1df1cfbe
intinazca <60673220+intinazca@users.noreply.github.com>
2020-05-21 19:21:27 -0500

    comind corredigo del anterior
    En este comind se fuciona el anterior comind que hice mas el de juan, no habia actualizado mi proyecto.

--- Classy/src/control/ControlCurso.java
+++ Classy/src/control/ControlCurso.java
@@ -14,21 +14,12 @@ public class ControlCurso {
 
         return t;
     }
-
+//esto permite la conexion con las publicaciones
     public LinkedList<Curso> consultarTodoCurso() {
         LinkedList<Curso> listacurso = new LinkedList<>();
         Curso objcurso = new Curso();
-        String sql = "select * from Cursos";
+        String sql = "SELECT * FROM categorias;";
         listacurso = objcurso.consultAllCurso(sql);
         return listacurso;
-        
-        /*
-        String sql = "select c.id_curso as 'Id', c.nombre_curso as 'Cursos', c.descripcion_curso as 'Descripcion', "
-                + "a.nombre1_profesor as 'Nombre docente', a.apellido1_profesor as 'Apellido', "
-                + "b.nombre_categoria as 'Categoria' from CURSOS as c "
-                + "inner join PROFESORES as a on a.id_profesor = c.id_profesorF "
-                + "inner join Categorias as b on b.id_categoria = c.id_categoriaF;";
-        */
     }
-
 }
